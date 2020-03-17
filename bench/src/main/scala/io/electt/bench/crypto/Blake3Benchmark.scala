a4c941a7439ea47ccfe2a6bd02472babab3ded14
Kirill A. Korinsky <kirill@korins.ky>
2020-03-17 13:24:57 +0100

    Added benchmarks for Blake3
    For the records, the results
    ```
    [info] Benchmark             (dataLen)  (hashLen)  Mode  Cnt     Score     Error  Units
    [info] Blake3Benchmark.hash          0         32  avgt    5    24.497 ±   0.395  us/op
    [info] Blake3Benchmark.hash          0         64  avgt    5    30.700 ±   1.091  us/op
    [info] Blake3Benchmark.hash          0        128  avgt    5    47.301 ±   1.060  us/op
    [info] Blake3Benchmark.hash          0        256  avgt    5    75.673 ±   2.389  us/op
    [info] Blake3Benchmark.hash         64         32  avgt    5    25.559 ±   0.940  us/op
    [info] Blake3Benchmark.hash         64         64  avgt    5    31.064 ±   0.969  us/op
    [info] Blake3Benchmark.hash         64        128  avgt    5    46.258 ±   0.654  us/op
    [info] Blake3Benchmark.hash         64        256  avgt    5    74.201 ±   0.876  us/op
    [info] Blake3Benchmark.hash        256         32  avgt    5    50.921 ±   0.956  us/op
    [info] Blake3Benchmark.hash        256         64  avgt    5    57.183 ±   1.967  us/op
    [info] Blake3Benchmark.hash        256        128  avgt    5    67.986 ±   0.767  us/op
    [info] Blake3Benchmark.hash        256        256  avgt    5   101.227 ±   1.621  us/op
    [info] Blake3Benchmark.hash       1024         32  avgt    5   140.106 ±   3.444  us/op
    [info] Blake3Benchmark.hash       1024         64  avgt    5   149.987 ±  13.272  us/op
    [info] Blake3Benchmark.hash       1024        128  avgt    5   164.310 ±   6.772  us/op
    [info] Blake3Benchmark.hash       1024        256  avgt    5   189.870 ±   3.280  us/op
    [info] Blake3Benchmark.hash      16384         32  avgt    5  2299.920 ±  88.475  us/op
    [info] Blake3Benchmark.hash      16384         64  avgt    5  2349.061 ± 163.103  us/op
    [info] Blake3Benchmark.hash      16384        128  avgt    5  2321.992 ±  32.533  us/op
    [info] Blake3Benchmark.hash      16384        256  avgt    5  2307.690 ±  91.906  us/op
    ```

---
+++ bench/src/main/scala/io/electt/bench/crypto/Blake3Benchmark.scala
@@ -0,0 +1,31 @@
+package io.electt.bench.crypto
+
+import io.electt.shared.crypto.Blake3
+import org.openjdk.jmh.annotations._
+
+import scala.util.Random
+
+@State(Scope.Benchmark)
+class Blake3Benchmark {
+  @Param(Array("0", "64", "256", "1024", "16384"))
+  var dataLen: Int = 0
+  var data: Array[Byte] = Array()
+
+  @Param(Array("32", "64", "128", "256"))
+  var hashLen: Int = 0
+  var hashBytes: Array[Byte] = Array()
+
+  @Setup
+  def setup(): Unit = {
+    val random = new Random()
+    data = Array.fill(dataLen)(0)
+    random.nextBytes(data)
+    hashBytes = Array.fill[Byte](hashLen)(0)
+  }
+
+  @Benchmark
+  def hash(): Unit =
+    Blake3.newHasher()
+      .update(data)
+      .finalize(hashBytes)
+}
