25da27a8e05f678ff7e5d9aed24068ac266192f9
Kirill A. Korinsky <kirill@korins.ky>
2020-11-21 22:56:47 +0100

    Added much more test vectors
    This commit:
     - split test vectors to official and additional;
     - syncronized official with fdce1eb7833cbb9bbc98a3c384b4bc240006aa44
     - introdused square test case

---
+++ shared/src/test/scala/ky/korins/blake3/AdditionalTestVectorsTest.scala
@@ -0,0 +1,59 @@
+package ky.korins.blake3
+
+import org.scalatest.matchers.should
+import org.scalatest.wordspec.AnyWordSpec
+
+class AdditionalTestVectorsTest extends AnyWordSpec with should.Matchers {
+  "All possibly bytes where inputLen" when {
+    val testVector = new TestVector(
+      testKey = "Ao largo Ainda arde a barca da fantasia o meu sonho acaba tarde deixa a alma de vigia",
+      testContext = "Scala-BLAKE3 2020-11-21 17:34:42 additional test vectors context",
+      inputLimit = 255
+    )
+
+    "1024" in {
+      testVector.runCase(
+        inputLen = 1024,
+        outputLen = 32,
+        hash = "3967a51c259d8262df62f586891923030f98ebefe67f867a11a3e8db8e11f957",
+        keyedHash = "c9807ec8f612f5e700176f9fe005e82e4180eeb4c660f4fe84ffff948f5d36ed",
+        deriveKeyHash = "f56c3da85c7c266ddf811a26d7381ae7420505d27362ee79c5157bfd240e249d"
+      )
+      testVector.runSquareCase(1024)
+    }
+
+    "2048" in {
+      testVector.runCase(
+        inputLen = 2048,
+        outputLen = 32,
+        hash = "a33417bf516bd6a9fa01bed9c70780cf8ae48f864f021ca72275844ad458cf1e",
+        keyedHash = "5dcc76347ef09ff4bd0062d41e8553badf765d5211aa84171bc94aa8fba075e9",
+        deriveKeyHash = "8c12563793ea5ee026fa3e44276f497a29d99940dd32862f93f97170c71ffcdc"
+      )
+      testVector.runSquareCase(2048)
+    }
+
+    "16665" in {
+      testVector.runCase(
+        inputLen = 16665,
+        outputLen = 32,
+        hash = "d3d6c3a478b4110511356fc16827092922f675ace34bc37a2cb2b30dc18a6145",
+        keyedHash = "b5eb4aae1fe88b0b9ad4afdc0f29ebd64edda72608d4fca854f06b9831b55bed",
+        deriveKeyHash = "2773641596d84c2384a5239256dc20f3910efe1f239bbcb91f91906457d691c5"
+      )
+      testVector.runSquareCase(16665)
+    }
+
+
+    "123456" in {
+      testVector.runCase(
+        inputLen = 123456,
+        outputLen = 32,
+        hash = "fe0122766f6f28a77e3552a095d5f79d32f4bbc354f5a0d84a06259b3a50e9eb",
+        keyedHash = "10e6f69a82fb36017b17aa26d6d62011400c833c03d71a9c880b76e17fe15260",
+        deriveKeyHash = "c0065f11a49d9508d1645f3a2bba64644ecf91fdda02a0293777901295ad4ecc"
+      )
+      testVector.runSquareCase(123456)
+    }
+  }
+}
---
+++ shared/src/test/scala/ky/korins/blake3/TestVector.scala
@@ -0,0 +1,135 @@
+package ky.korins.blake3
+
+import ky.korins.blake3
+import org.scalatest.Assertion
+import org.scalatest.matchers.should
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
+import scala.language.implicitConversions
+
+class TestVector(
+  val testKey: String,
+  val testContext: String,
+  val inputLimit: Int = 251
+) extends should.Matchers {
+
+  final val updateMethods: Seq[(Hasher, Array[Byte]) => Unit] = Seq(
+    { (hasher, input) =>
+      hasher.update(input)
+    },
+    { (hasher, input) =>
+      input.foreach(hasher.update)
+    },
+    { (hasher, input) =>
+      val bos = new ByteArrayInputStream(input)
+      hasher.update(bos)
+    },
+    { (hasher, input) =>
+      val bb = ByteBuffer.wrap(input)
+      hasher.update(bb)
+    }
+  )
+
+  private def byte2hex(bytes: Array[Byte]): String = {
+    val sb = new StringBuilder(bytes.length * 2)
+    var i = 0
+    while (i < bytes.length) {
+      sb.append("%02x".format(bytes(i) & 0xff))
+      i += 1
+    }
+    sb.toString()
+  }
+
+  final val doneMethods: Seq[(Hasher, Int) => String] = Seq(
+    { (hasher, outputLen) =>
+      byte2hex(hasher.done(outputLen))
+    },
+    { (hasher, outputLen) =>
+      hasher.doneBase16(outputLen).toLowerCase
+    },
+    { (hasher, outputLen) =>
+      hasher.doneHex(2 * outputLen)
+    },
+    { (hasher, outputLen) =>
+      val bos = new ByteArrayOutputStream()
+      hasher.done(bos, outputLen)
+      byte2hex(bos.toByteArray)
+    },
+    { (hasher, outputLen) =>
+      val bb = ByteBuffer.allocate(outputLen)
+      hasher.done(bb)
+      byte2hex(bb.array())
+    }
+
+  )
+
+  def runCase(
+    inputLen: Int,
+    outputLen: Int,
+    hash: String,
+    keyedHash: String,
+    deriveKeyHash: String
+  ): Seq[Assertion] = {
+    val input = (0 until inputLen).map(_ % inputLimit).map(_.toByte).toArray
+    for {
+      update <- updateMethods
+      done <- doneMethods
+    } yield {
+      val hasher = Blake3.newHasher()
+      val keyedHasher = Blake3.newKeyedHasher(testKey.getBytes(StandardCharsets.US_ASCII).take(blake3.KEY_LEN))
+      val deriveHeyHasher = Blake3.newDeriveKeyHasher(testContext)
+
+      update(hasher, input)
+      update(keyedHasher, input)
+      update(deriveHeyHasher, input)
+
+      done(hasher, outputLen) should be(hash)
+      done(keyedHasher, outputLen) should be(keyedHash)
+      done(deriveHeyHasher, outputLen) should be(deriveKeyHash)
+    }
+  }
+
+  def runSquareCase(len: Int): Assertion = {
+    val input = (0 until len).map(_ % inputLimit).map(_.toByte).toArray
+    val hashers: Seq[((Hasher, Int) => String, (Hasher, Hasher, Hasher))] = for {
+      update <- updateMethods
+      done <- doneMethods
+    } yield {
+      val hasher = Blake3.newHasher()
+      val keyedHasher = Blake3.newKeyedHasher(testKey.getBytes(StandardCharsets.US_ASCII).take(blake3.KEY_LEN))
+      val deriveKeyHasher = Blake3.newDeriveKeyHasher(testContext)
+
+      update(hasher, input)
+      update(keyedHasher, input)
+      update(deriveKeyHasher, input)
+
+      (done, (hasher, keyedHasher, deriveKeyHasher))
+    }
+
+    val hashes: Seq[(String, String, String)] = hashers
+      .map { case (done, (hasher, keyedHasher, deriveHeyHasher)) =>
+        (done(hasher, len), done(keyedHasher, len), done(deriveHeyHasher, len))
+      }
+
+    val reducer: (((String, String, String), (String, String, String)) => (String, String, String)) = {
+      case (
+        (leftHash: String, leftKeyedHash: String, leftDeriveKeyHash: String),
+        (rightHash: String, rightKeyedHash: String, rightDeriveKeyHash: String)) =>
+
+        leftHash shouldBe rightHash
+        leftKeyedHash shouldBe rightKeyedHash
+        leftDeriveKeyHash shouldBe rightDeriveKeyHash
+
+        (rightHash, rightKeyedHash, rightDeriveKeyHash)
+    }
+
+    val (hash: String, keyedHash: String, deriveKeyHash: String) = hashes
+      .reduce(reducer)
+
+    hash.length shouldBe 2 * len
+    keyedHash.length shouldBe 2 * len
+    deriveKeyHash.length shouldBe 2 * len
+  }
+}
